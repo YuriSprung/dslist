29688c3abe21cfd24c127d3b1610346a900f8cdd
Yuri <gaby19sprung@gmail.com>
2024-11-12 21:04:02 -0300

    GET game by id

--- src/main/java/com/yurisprung/dslist/services/GameService.java
+++ src/main/java/com/yurisprung/dslist/services/GameService.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.yurisprung.dslist.dto.GameDTO;
 import com.yurisprung.dslist.dto.GameMinDTO;
 import com.yurisprung.dslist.entities.Game;
 import com.yurisprung.dslist.repositories.GameRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class GameService {
@@ -15,8 +18,15 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@org.springframework.transaction.annotation.Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@org.springframework.transaction.annotation.Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
 	}
 }
